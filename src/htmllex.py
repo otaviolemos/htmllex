0c1810377d6054b6225dbe38115329e6a167497d
Otavio Lemos <otaviolemos@gmail.com>
2022-06-09 20:37:10 -0300

    feat: two more tests

--- src/htmllex.py
+++ src/htmllex.py
@@ -2,47 +2,42 @@ class HtmlLex:
     def __init__(self, text):
         self.text = text
         self.result = ""
-        self.current_index = 0
+        self.index = 0
 
-    def _get_tag_name(self, tag):
-      tag_name = ""
-      index = 0
-      while tag[index] != ">" and tag[index] != " ":
-          tag_name += tag[index]
-          index += 1  
-      return tag_name, index
-
-    def _get_attribute_name(self, tag, index):
-      attribute_name = ""
-      int_index = index
-      while tag[int_index] != "=":
-          attribute_name += tag[int_index]
-          int_index += 1  
-      return attribute_name, int_index
-
-    def _get_attribute_value(self, tag, index):
-      attribute_value = ""
-      int_index = index
-      while tag[int_index] != "\"":
-          attribute_value += tag[int_index]
-          int_index += 1
-      return attribute_value, int_index
+    def _get_token(self, tag, charset):
+      token = ""
+      while tag[self.index] not in charset:
+          token += tag[self.index]
+          self.index += 1  
+      return token
 
     def process(self):
-          tags_list = self.text.split("<")
-          for tag in tags_list:
-            if (len(tag) == 0):
-                continue
-            tag_name, index = self._get_tag_name(tag)
-            if(tag_name[0] != "/"):
-              self.result = self.result + tag_name
-            if (tag[index] == " "):
-              self.result = self.result + "\n-> "
-              attribute_name, index = self._get_attribute_name(tag, index+1)
-              self.result = self.result + attribute_name
-              attribute_value, _ = self._get_attribute_value(tag, index+2)
-              self.result = self.result + " > " + attribute_value
+      self._strip_comments()
+      tags_list = self.text.split("<")
+      for tag in tags_list:
+        self.index = 0
+        if (len(tag) == 0):
+            continue
+        tag_name = self._get_token(tag, [">", " "])
+        if tag_name[0] == "/":
+            continue
+        self.result = self.result + tag_name + "\n"
+        while (tag[self.index] in (" ", "\n") ) and tag[self.index+1].isalpha():
+          self.result = self.result + "-> "
+          self.index += 1
+          attribute_name = self._get_token(tag, ["="])
+          self.result = self.result + attribute_name
+          self.index += 2
+          attribute_value = self._get_token(tag, ["\""])
+          self.result = self.result + " > " + attribute_value + "\n"
+          self.index += 1
+          
 
+    def _strip_comments(self):
+      while(self.text.find("<!--") != -1):
+        start_index = self.text.find("<!--")
+        end_index = self.text.find("-->")
+        self.text = self.text[:start_index] + self.text[end_index+3:]
 
     def get_result(self):
         return self.result
--- src/tests.py
+++ src/tests.py
@@ -3,26 +3,64 @@ from htmllex import HtmlLex
 
 class TestHtmlLex(unittest.TestCase):
     def test_empty_string(self):
-        text = ""
-        lex = HtmlLex(text)
-        lex.process()
-        self.assertEqual(lex.get_result(), "")
+      text = ""
+      lex = HtmlLex(text)
+      lex.process()
+      self.assertEqual(lex.get_result(), "")
 
     def test_single_tag(self):
-        text = "<html>"
-        lex = HtmlLex(text)
-        lex.process()
-        self.assertEqual(lex.get_result(), "html")
+      text = "<html>"
+      lex = HtmlLex(text)
+      lex.process()
+      self.assertEqual(lex.get_result(), "html\n")
 
     def test_single_tag_with_closing_tag(self):
-        text = "<html></html>"
-        lex = HtmlLex(text)
-        lex.process()
-        self.assertEqual(lex.get_result(), "html")
+      text = "<html></html>"
+      lex = HtmlLex(text)
+      lex.process()
+      self.assertEqual(lex.get_result(), "html\n")
 
     def test_single_tag_with_single_attribute(self):
-        text = "<object type=\"application/x-flash\"\>"
-        lex = HtmlLex(text)
-        lex.process()
-        self.assertEqual(lex.get_result(), 
-          "object\n-> type > application/x-flash")
+      text = "<object type=\"application/x-flash\"\>"
+      lex = HtmlLex(text)
+      lex.process()
+      self.assertEqual(lex.get_result(), 
+        "object\n-> type > application/x-flash\n")
+
+    def test_ignores_comment(self):
+      text = "<!-- <param name=\"movie\" value=\"your-file.swf\" /> -->"
+      lex = HtmlLex(text)
+      lex.process()
+      self.assertEqual(lex.get_result(), "")
+
+    def test_with_two_parameters(self):
+      text = "<object type=\"application/x-flash\" data=\"your-file.swf\"\>"
+      lex = HtmlLex(text)
+      lex.process()
+      self.assertEqual(lex.get_result(), 
+        "object\n-> type > application/x-flash\n-> data > your-file.swf\n")
+
+    def test_with_complete_html(self):
+      text = """<head>
+<title>HTML</title>
+</head>
+<object type="application/x-flash"
+data="your-file.swf"
+width="0" height="0">
+  <!-- <param name="movie" value="your-file.swf" /> -->
+  <param name="quality" value="high"/>
+</object>"""
+      lex = HtmlLex(text)
+      lex.process()
+      print(lex.get_result())
+#       self.assertEqual(lex.get_result(), 
+#         """head
+# title
+# object
+# -> type > application/x-flash
+# -> data > your-file.swf
+# -> width > 0
+# -> height > 0
+# param
+# -> name > quality
+# -> value > high""")
